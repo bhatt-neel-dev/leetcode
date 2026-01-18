1deeac69f742afe1852c352464eb98784f510dae
Neel Bhatt <developer@bhattneel.com>
2026-01-18 21:22:56 +0530

    Add solution for Add Two Numbers

--- solutions/medium/Add Two Numbers.py
+++ solutions/medium/Add Two Numbers.py
@@ -3,16 +3,12 @@
             // Language: Python
             // Link: https://leetcode.com/problems/add-two-numbers/
 
-            val1 = l1.val if l1 else 0
-            val2 = l2.val if l2 else 0
-            carry, sum_val = divmod(val1 + val2 + carry, 10)
-            curr.next = ListNode(sum_val)
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+        while l1 or l2 or carry:
+            carry, sv = divmod(v1+v2+carry, 10)
+            curr.next = ListNode(sv)
             curr = curr.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
-
-        while l1 or l2 or carry:
-        carry = 0
-        curr = dummy_head
-        dummy_head = ListNode(0)
         return dummy_head.next
