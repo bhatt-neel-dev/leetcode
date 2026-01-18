32863746a16c52e6f5b5729ecdb82beed1140fbd
Neel Bhatt <developer@bhattneel.com>
2026-01-18 20:58:11 +0530

    Add solution for Two Sum

--- solutions/easy/Two Sum.py
+++ solutions/easy/Two Sum.py
@@ -4,15 +4,11 @@
             // Link: https://leetcode.com/problems/two-sum/
 
         """
-        for i in range(len(nums)):
-        index_map = {}
-            if complement in index_map:
-                return index_map[complement], i
-            complement = target-nums[i]
-            index_map[nums[i]] = i
-        :rtype: List[int]
-        :type target: int
-        :type nums: List[int]
-        """
-class Solution(object):
-    def twoSum(self, nums, target):
+        idx={}
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in idx:
+        n=len(nums)
+                return idx[complement], i
+            idx[nums[i]] = i
+
